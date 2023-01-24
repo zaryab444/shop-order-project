19f486847f1f5a2d80f53e6eebc983acaf1013c5
zaryab <zaryabdeveloper49@gmail.com>
2023-01-25 01:24:46 +0500

    code update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
 
